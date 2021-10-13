c4ec6b2c9f7acddebe80f50de859518ecaf0c291
Akshay Bahadur <akshaybahadur21@gmail.com>
2021-10-13 07:48:52 +0530

    Update scourse.py

--- scourse.py
+++ scourse.py
@@ -3,41 +3,38 @@ Group 20: SCOURSE
 Members : Abhinaav Singh(abhinaas), Akshay Bahadur(akshayba), Chirag Huria(churia), Naman Arora(namana)
 
 """
-import argparse
-
-from src.etl.etl import performETL
-from src.utils.print_utils import print_banner, print_menu, print_role_menu, print_skillset_menu, str2bool
-from src.algo.course_similarity import course_sim
 
 import warnings
+
+from src.algo.course_similarity import course_sim
+from src.etl.etl import performETL
+from src.utils.print_utils import print_banner, print_menu, print_role_menu, print_skillset_menu
+
 warnings.filterwarnings("ignore")
 
 
 class SCOURSE:
-    def __init__(self, perFormETL):
+    def __init__(self):
         print_banner()
-        self.perFromETL = perFormETL
 
     def run(self):
-        if self.perFromETL:
-            performETL()
         while True:
             print_menu()
             choice = input()
-            if choice == '2':
+            if choice == '3':
                 break
-            print_role_menu()
-            role = input()
-            print_skillset_menu()
-            skill = input()
-            course_sim("{} {}".format(role, skill))
+            elif choice == '2':
+                performETL()
+            elif choice == '1':
+                print_role_menu()
+                role = input()
+                print_skillset_menu()
+                skill = input()
+                course_sim("{} {}".format(role, skill))
+            else:
+                print("Please select an appropriate input.")
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-performETL', "--performETL", help="Perform ETL",
-                        type=str2bool, default=False)
-    args = parser.parse_args()
-    perform_etl = args.performETL
-    scourses = SCOURSE(perform_etl)
+    scourses = SCOURSE()
     scourses.run()
